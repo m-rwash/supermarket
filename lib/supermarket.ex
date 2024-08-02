cce7bd0a51d3a22df73cd39cd1f2e687576786c0
m-rwash <m.rwash593@gmail.com>
2024-08-02 19:19:22 +0200

    feat: trivial checkout calculation

--- lib/supermarket.ex
+++ lib/supermarket.ex
@@ -2,4 +2,12 @@ defmodule Supermarket do
   @moduledoc """
   Documentation for `Supermarket`.
   """
+
+  alias Decimal, as: D
+
+  def checkout(%{products: products}) do
+    products
+    |> Enum.reduce(D.new(0), fn product, acc -> D.add(acc, product.price) end)
+    |> D.to_string()
+  end
 end
